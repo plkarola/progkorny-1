366b72f7b47566694435d67d5a26ba587aebc4e4
chete <chete@172.17.24.253>
2016-05-17 00:52:00 +0200

    fixed_parser

--- src/main/java/uzletilogika/XMLParserImp.java
+++ src/main/java/uzletilogika/XMLParserImp.java
@@ -12,8 +12,7 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
-import org.junit.Rule;
-import org.junit.rules.TemporaryFolder;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
